33883b2aebcd5328f7b21a9d767932631c3d8ac5
Philippe Guay <Philippe.Guay@videri.com>
2015-10-29 17:09:56 -0400

    adding restart branch checkout + ember restart

--- senv.sh
+++ senv.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+###
+# User script to ease the process of changing environment settings
+# When working with a local development proxy setup
+# Also takes care of checking out the appropriate branch if you have a feature branch for that
+###
 
+repoPath="$HOME/git"
+techopsFolderName="demo-ops-console"
+TARGET_ENV="dev"
+CONFIG_PATH="$HOME/nginx.conf"
+DEFAULT_BRANCH="master"
+ENVS=( qa dev sandpit )
+### HELPER METHODS
 getCurrentEnv () {
- ENVS=( qa dev sandpit )
  for env in "${ENVS[@]}"
  do
-   #echo "VALUE:`grep -c "${env}" ${file}`"
-   if [[ `grep -c "${env}" ${file}` > 0 ]]
+   #echo "VALUE:`grep -c "${env}" ${CONFIG_PATH}`"
+   if [[ `grep -c "${env}" ${CONFIG_PATH}` > 0 ]]
    then
       echo "${env}"
    fi
  done 
 }
+
 restartServer () {
    configPath=$1
    nginx -s stop
@@ -18,43 +30,84 @@ restartServer () {
    nginx -c ${configPath}
 }
 
-if [ $# -eq 0 ]
-then
-  ### 0 Args: All defaults
- targetEnv="dev"
- file="/Users/philippeguay/nginx.conf"
-elif [ $# -eq 1 ]
-then
- ### 1 Arg: environment
- file="/Users/philippeguay/nginx.conf"
- targetEnv=$1
-elif [ $# -eq 2 ]
-then
-  ### 2 Args: $1: Environment, $2: config path
-  targetEnv=$1
-  file=$2 
-else
-  echo "Invalid number of arguments"
-fi
+#### OPTION PARSING
+while [[ $# > 1 ]]
+do
+key="$1"
+case $key in
+  -c|--conf|--config)
+  CONFIG_PATH="$2"
+  shift
+  ;;
+  -b|--branch)
+  BRANCH="$2"
+  shift
+  ;;
+  -e|--env)
+   TARGET_ENV="$2"
+   shift 
+  ;;
+esac
+done
 
+####
+echo "-----ENVIRONMENT SWITCH---"
 currentEnv=$(getCurrentEnv)
-if [ ${currentEnv} == ${targetEnv} ]
+if [ ${currentEnv} == ${TARGET_ENV} ]
 then
-   echo "${targetEnv} is already the current environment."
+   echo "${TARGET_ENV} is already the current environment."
 else
-   if [ ! -f "${file}.${targetEnv}" ]
+   if [ ! -f "${CONFIG_PATH}.${TARGET_ENV}" ]
    then
-      echo "Changing from [${currentEnv}] to [${targetEnv}] using config: [${file}]"
-      sed -i '' "s/${currentEnv}/${targetEnv}/g" ${file}
+      echo "Changing from [${currentEnv}] to [${TARGET_ENV}] using config: [${CONFIG_PATH}]"
+      sed -i '' "s/${currentEnv}/${TARGET_ENV}/g" ${CONFIG_PATH}
    else
-     echo "Config for ${targetEnv} was found at path ${file}.${targetEnv}"
+     echo "Config for ${TARGET_ENV} was found at path ${CONFIG_PATH}.${TARGET_ENV}"
    fi
 fi
 
-echo "Restarting Nginx Proxy server for ENV:${targetEnv}..."
-if [ -f "${file}.${targetEnv}" ]
+###
+echo "------BRANCH CHECKOUT----"
+checkoutPath="${repoPath}/${techopsFolderName}"
+cd ${checkoutPath}
+git rev-parse --verify ${BRANCH}
+if [ $? == 0 ] && [ -z "$BRANCH" ]
 then
-   restartServer "${file}.${targetEnv}"
+   echo "Branch ${BRANCH} was found."
 else
-   restartServer "${file}"
+   git rev-parse --verify ${TARGET_ENV}
+   if [ $? == 0 ]
+   then
+     echo "An environment specific branch was found. Will use ${TARGET_ENV} branch"
+     BRANCH=${TARGET_ENV}
+   else
+     echo "No branch specified or unknown branch ${BRANCH}.  Will use ${DEFAULT_BRANCH}"
+     BRANCH=${DEFAULT_BRANCH}
+   fi
 fi
+echo "Checking out ${BRANCH} at path ${checkoutPath}"
+git checkout ${BRANCH}
+
+###
+echo "------SERVER RESTART-----"
+echo "Restarting Nginx Proxy server for ENV:${TARGET_ENV}..."
+if [ -f "${CONFIG_PATH}.${TARGET_ENV}" ]
+then
+   restartServer "${CONFIG_PATH}.${TARGET_ENV}"
+else
+   restartServer "${CONFIG_PATH}"
+fi
+
+###
+echo "-----CLIENT SERVER RESTART-----"
+emberProc=$(pgrep 'ember')
+echo "Ember Proc [${emberProc}]"
+if [ "${emberProc}" != "" ]
+then
+  echo "Killing Ember process ${emberProc}"
+  kill -9 ${emberProc}
+fi
+echo "Restarting Ember server"
+cd "${repoPath}/${techopsFolderName}"
+ember s &
+
