3a5d9bd7b6fafcf495138da651399e1fbd708ee0
Philippe Guay <Philippe.Guay@videri.com>
2015-11-02 10:39:33 -0500

    # This is a combination of 6 commits. # The first commit's message is: few more options + better handling of client server restart
    # This is the 2nd commit message:
    
    adding host replacement + server config
    
    # This is the 3rd commit message:
    
    bit of cleanup
    
    # This is the 4th commit message:
    
    anonymous template
    
    # This is the 5th commit message:
    
    adding oauth service
    
    # This is the 6th commit message:
    
    cleanup

--- mvToPath.sh
+++ mvToPath.sh
@@ -1,3 +1,8 @@
 echo "Moving '$HOME/user-scripts/senv.sh' to '/usr/bin/senv/'"
 sudo cp $HOME/user-scripts/senv.sh /usr/bin/senv
+echo "Moving '$HOME/user-scripts/config.sh' to $HOME/config.sh"
+sudo cp $HOME/user-scripts/config.sh $HOME/config.sh
+
+echo "Moving '$HOME/user-scripts/help.sh' to $HOME/help.sh"
+sudo cp $HOME/user-scripts/help.sh $HOME/help.sh
 
--- senv.sh
+++ senv.sh
@@ -1,135 +1,250 @@
 #!/bin/bash
-###
 # User script to ease the process of changing environment settings
-
 # Also takes care of checking out the appropriate branch if you have a feature branch for that
-###
 
-REPO_PATH="$HOME/git"
-TECHOPS_FOLDER="demo-ops-console"
-TARGET_ENV="dev"
-CONFIG_PATH="$HOME/nginx.conf"
-DEFAULT_BRANCH="master"
-ENVS=( qa dev sandpit )
-BRANCH=
-### HELPER METHODS
-getCurrentEnv () {
- #TODO: Interrogate current running Nginx Process to know if we are running an environment specific config. That will tell us the currentEnv more accurately. 
- for env in "${ENVS[@]}"
- do
-   #echo "VALUE:`grep -c "${env}" ${CONFIG_PATH}`"
-   if [[ `grep -c "${env}" ${CONFIG_PATH}` > 0 ]]
-   then
-      echo "${env}"
-   fi
- done 
+#IMPORTANT
+#You need to have a config file named config.sh in your $HOME path
+# This file should be based on config.sh.template and populated as needed
+
+
+#HELPER METHODS
+section () {
+  echo ""
+  echo "========== $1 =========="
+  echo ""
+}
+
+initScript () {
+  cd $HOME
+  if [ ! -f "$HOME/config.sh" ]
+  then
+    echo "Config file config.sh not found"
+    exit 0
+  else
+    echo "loading config config.sh"
+    source config.sh
+  fi
+  if [ ! -f "$HOME/help.sh" ]
+  then
+    echo "No help file found at $HOME/help.sh"
+    exit 0
+  else
+    source help.sh
+  fi
+}
+
+generateConfigFromTemplate () {
+  # We validate that a template file is present
+  if [ ! -f "${CONFIG_PATH}.${TARGET_ENV}.template" ]
+  then
+    echo "ERR -- Environment Specific file not found. If you want to have custom hosts you should specify a template config for your environment"
+    exit 0
+  else
+    echo "HOST REPLACE FILE [${CONFIG_PATH}.${TARGET_ENV}.template]"
+    # Cleaning up existing temp file
+    if [ -f "${CONFIG_PATH}.${TARGET_ENV}.tmp" ]
+    then
+      echo "CLEANUP -> rm ${CONFIG_PATH}.${TARGET_ENV}.tmp"
+      rm "${CONFIG_PATH}.${TARGET_ENV}.tmp"
+    fi
+
+    # Generating temp work file
+    echo "TMP FILE -> ${CONFIG_PATH}.${TARGET_ENV}.tmp"
+    cp "${CONFIG_PATH}.${TARGET_ENV}.template" "${CONFIG_PATH}.${TARGET_ENV}.tmp"
+
+    replaceHosts
+
+    # Replacing current file by this work file
+    echo "COPY ${CONFIG_PATH}.${TARGET_ENV}.tmp -> ${CONFIG_PATH}.${TARGET_ENV}"
+    cp "${CONFIG_PATH}.${TARGET_ENV}.tmp" "${CONFIG_PATH}.${TARGET_ENV}"
+
+  fi
+
 }
 
 restartServer () {
-   configPath=$1
-   sudo nginx -s stop
-   echo "Booting Nginx using config ${configPath}"
-   sudo nginx -c ${configPath}
+  configPath=$1
+  sudo nginx -s stop
+  echo "BOOT NGINX... CONFIG [${configPath}]"
+  sudo nginx -c ${configPath}
 }
 
-#### OPTION PARSING
-while [[ $# > 1 ]]
+runAsUser () {
+  echo "RUNAS [${USER}] -> $1"
+  su ${USER} -c "cd ${CHECKOUT_PATH}; $1"
+}
+
+checkoutBranch () {
+  branch=$1
+  echo "CHECKOUT [${branch}]"
+  runAsUser "git checkout ${branch}"
+}
+
+getCurrentBranch () {
+  git branch | sed -n '/\* /s///p'
+}
+
+replaceHosts () {
+
+  # Check if its the local flag has been set for this host. Override TARGET_ENV to LOCAL if yes
+  # Will also need to uppercase TARGET_ENVS
+  upperEnv=`echo "${TARGET_ENV}" | awk '{print toupper($0)}'`
+  echo "REPLACEHOST FILE [${CONFIG_PATH}.${TARGET_ENV}.tmp]"
+  for hostname in "${SERVICES[@]}"
+  do
+    localhost="LOCAL_$hostname"
+    localhost_host="LOCAL_${hostname}_HOST"
+    env_host="${upperEnv}_${hostname}_HOST"
+    if [ "${!localhost}" == "true" ]
+    # Using commas for delimiters so that we don't have to escape hosts that use slashes
+    then
+      echo "REPLACEHOST %${hostname}_HOST% --> ${!localhost_host}"
+      sed -i '' "s,%${hostname}_HOST%,${!localhost_host},g" "${CONFIG_PATH}.${TARGET_ENV}.tmp"
+    else
+      echo "REPLACEHOST %${hostname}_HOST% --> ${!env_host}"
+      sed -i '' "s,%${hostname}_HOST%,${!env_host},g" "${CONFIG_PATH}.${TARGET_ENV}.tmp"
+    fi
+  done
+}
+
+########################################################################################
+section "INIT"
+initScript
+
+#TODO: parse ":"-delimited string to represent services to run locally
+while [[ $# > 0 ]]
 do
 key="$1"
 case $key in
   -h|--help)
-  echo "[-h --help] Help  [-c --conf --config] Nginx Config path [-b --branch] Branch to checkout [-e --env] target environment"
-  shift
+    showHelp
   ;;
   -c|--conf|--config)
-  CONFIG_PATH="$2"
-  shift
+    CONFIG_PATH="$2"
+    shift
   ;;
   -b|--branch)
-  BRANCH="$2"
-  shift
+    BRANCH="$2"
+    shift
   ;;
   -e|--env)
-  TARGET_ENV="$2"
-  shift 
+    TARGET_ENV="$2"
+    shift
   ;;
-  -f)
-  CREATE_BRANCH="true"  
+  -s|--same-br)
+    SAME_BRANCH="true"
+    shift
+  ;;
+  --create-br)
+    CREATE_BRANCH="true"
+    shift
+  ;;
+  -r)
+    RESTART_CLIENT="true"
+    shift
+  ;;
+  --srvc)
+    LOCAL_CENTRAL="true"
+    shift
+  ;;
+  --srvlib)
+    LOCAL_LIBRARY="true"
+    shift
+  ;;
+  --srvs)
+    LOCAL_SCHEDULER="true"
+    shift
+  ;;
+  --srvd)
+    LOCAL_DATASOURCE="true"
+    shift
+  ;;
+  --srvr)
+    LOCAL_REPORTER="true"
+    shift
+  ;;
+  --srvl)
+    LOCAL_LAYOUTENGINE="true"
+    shift
+  ;;
+  --srva)
+    AGENT_HOST="true"
+    shift
+  ;;
+  --srvo)
+    LOCAL_OAUTH="true"
+    shift
+  ;;
+  *)
+    shift
+  ;;
 esac
 done
 
-####
-echo "-----ENVIRONMENT SWITCH---"
-currentEnv=$(getCurrentEnv)
-if [ ${currentEnv} == ${TARGET_ENV} ]
-then
-   echo "${TARGET_ENV} is already the current environment."
-else
-   if [ ! -f "${CONFIG_PATH}.${TARGET_ENV}" ]
-   then
-      echo "Changing from [${currentEnv}] to [${TARGET_ENV}] using config: [${CONFIG_PATH}]"
-      sed -i '' "s/${currentEnv}/${TARGET_ENV}/g" ${CONFIG_PATH}
-   else
-     echo "Config for ${TARGET_ENV} was found at path ${CONFIG_PATH}.${TARGET_ENV}"
-   fi
-fi
+section "ENVIRONMENT SWITCH"
+generateConfigFromTemplate
+cd ${CHECKOUT_PATH}
+initialBranch=$(getCurrentBranch)
+echo "initialBranch ${initialBranch}"
 
-###
-echo "------BRANCH CHECKOUT----"
-checkoutPath="${REPO_PATH}/${TECHOPS_FOLDER}"
-cd ${checkoutPath}
-echo "BRANCH: ${BRANCH}"
+########################################################################################
+section "BRANCH CHECKOUT"
+
 git show-ref --verify --quiet refs/heads/${BRANCH}
-if [ $? == 0 ] && [ "$BRANCH" != "" ]
+res=$?
+if [ "${SAME_BRANCH}" != "true" ]
 then
-   echo "Branch ${BRANCH} was found."
-else
-   git show-ref --verify --quiet refs/heads/${BRANCH}
-   if [ $? == 0 ]
-   then
-     echo "An environment specific branch was found. Will use ${TARGET_ENV} branch"
-     BRANCH=${TARGET_ENV}
-   else
-     if [ "${CREATE_BRANCH}" == "true" ]
-     then
-       echo "creating new branch ${BRANCH}"
-       git branch ${BRANCH}
-     else
-       echo "No branch specified or unknown branch ${BRANCH}.  Will use ${DEFAULT_BRANCH}"
-       BRANCH=${DEFAULT_BRANCH}
-     fi
-   fi
-fi
-echo "Checking out ${BRANCH} at path ${checkoutPath}"
-git checkout ${BRANCH}
-
-###
-echo "------SERVER RESTART-----"
-echo "Restarting Nginx Proxy server for ENV:${TARGET_ENV}..."
-if [ -f "${CONFIG_PATH}.${TARGET_ENV}" ]
-then
-   restartServer "${CONFIG_PATH}.${TARGET_ENV}"
-else
-   restartServer "${CONFIG_PATH}"
-fi
-
-###
-echo "-----CLIENT SERVER RESTART-----"
-if [ ${BRANCH} == ${TARGET_ENV} ]
-then
- echo "No need to restart Ember already on ${TARGET_ENV}"
-else
-  emberProc=$(pgrep 'ember')
-  echo "Ember Proc [${emberProc}]"
-  if [ "${emberProc}" != "" ]
+  if [ "${res}" == "0" ]
   then
-    echo "Killing Ember process ${emberProc}"
-    kill -9 ${emberProc}
+    checkoutBranch ${BRANCH}
+  else
+    if [ "${CREATE_BRANCH}" == "true" ]
+    then
+      echo "creating new branch ${BRANCH}"
+      runAsUser "git checkout ${DEFAULT_BRANCH}"
+      runAsUser "git pull origin ${DEFAULT_BRANCH}"
+      runAsUser "git branch ${BRANCH}"
+      checkoutBranch ${BRANCH}
+    else
+      git show-ref --verify --quiet refs/heads/${TARGET_ENV}
+      if [ $? == 0 ]
+      then
+        echo "An environment specific branch was found. Will use ${TARGET_ENV}"
+        checkoutBranch ${TARGET_ENV}
+      else
+        echo "No branch specified or unknown branch ${BRANCH}. Will use ${DEFAULT_BRANCH}"
+        checkoutBranch ${DEFAULT_BRANCH}
+      fi
+    fi
   fi
-  echo "Installing dependencies (npm + bower)"
-  npm install && bower install
-  echo "Restarting Ember server"
-  cd "${REPO_PATH}/${TECHOPS_FOLDER}"
-  ember s &
+else
+  echo "SAMEBRANCH Requested. Using [$(getCurrentBranch)]"
 fi
+########################################################################################
+section "NGINX SERVER RESTART [${TARGET_ENV}]"
+restartServer "${CONFIG_PATH}.${TARGET_ENV}"
 
+########################################################################################
+section "CLIENT SERVER RESTART"
+currentBranch=$(getCurrentBranch)
+emberPID=$(pgrep 'ember')
+if [ "$RESTART_CLIENT" == "true" ] || [ "${initialBranch}" != "${currentBranch}" ]
+then
+  if [ "${emberPID}" != "" ]
+  then
+    echo "KILL EMBER PROC [${emberPID}]"
+    kill -9 ${emberPID}
+  fi
+  runAsUser "npm install && bower install"
+  runAsUser "cd ${REPO_PATH}/${TECHOPS_FOLDER}"
+  runAsUser "ember s &"
+else
+  if [ "${emberPID}" == "" ]
+  then
+    echo "No ember process detected. Launching ember app."
+    runAsUser "ember s &"
+  else
+    echo "NORESTART -- BRANCHES (init: ${initialBranch}, current: ${currentBranch})"
+    echo "EMBER PID [${emberPID}]"
+  fi
+fi
