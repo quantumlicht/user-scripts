9678d5beb43c8c5c7b12ca6a53e9f16b41730bd9
Philippe Guay <pguay@elementai.com>
2017-07-24 11:03:23 -0400

    Separate scripts in folders

--- export_ssl.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo ""
-echo "exporting SSL key"
-export SSL_KEY=/Users/philippeguay/git/demo-ops-console/ssl/server.key
-echo "exporting SSL Cert"
-export SSL_CERT=/Users/philippeguay/git/demo-ops-console/ssl/server.crt
