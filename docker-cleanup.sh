96658211486c38cc470f31ea2f47f52cc7386bee
Philippe Guay <pguay@elementai.com>
2018-02-19 10:32:07 -0500

    move files to something safer than ~

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
+# docker system prune -a
+volumes=$(docker volume ls -qf dangling=true)
+echo "volumes: [${volumes}]"
+if [ "$volumes" ]; then
+  docker volume rm $volumes
+fi
 
-docker volume rm $(docker volume ls -qf dangling=true) && \
-docker rmi -f $(docker images -qf dangling=true) && \
-docker rm $(docker ps -q -f 'status=exited') && \
-docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
+images=$(docker images -qf dangling=true)
+echo "images: [$images]"
+if [ "$images" ]; then
+  docker rmi -f $images
+fi
+
+exited_processes=$(docker ps -q -f 'status=exited')
+echo "exited_processes: [$exited_processes]"
+if [ "$exited_processes" ]; then
+  docker rm $exited_processes
+fi
+
+none_images=$(docker images | grep "^<none>" | awk "{print $3}")
+echo "none images: [$none_images]"
+if [ "$none_images" ]; then
+  docker rmi $none_images
+fi
+
+
+docker rm $(docker ps -a -q)
 
